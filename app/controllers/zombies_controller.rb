bb52f9a98385ae885cceef443ab22c124f8b6878
Steve Brewer <steve.brewer@healios.org.uk>
2018-09-08 12:40:01 +0300

    Zombies can edit zombies

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -1,5 +1,27 @@
 class ZombiesController < ApplicationController
+  before_action :find_zombie, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @zombie.update_attributes(zombie_params)
+      redirect_to zombie_path(@zombie)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_zombie
     @zombie = Zombie.find(params[:id])
+  end
+
+  def zombie_params
+    params.require(:zombie).permit(:name, :bio)
   end
 end
